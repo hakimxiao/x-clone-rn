6b7dc0c758227d3fb5414aaeead54aa809d0fec7
hakimxiao <alhakimabusolihin@gmail.com>
2025-11-27 16:26:30 +0700

    Setup Tailwindcss :    - Menggunakan native wind untuk mengintegrasikan tilwind pada project react native.

---
+++ mobile/app/index.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Edit app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
